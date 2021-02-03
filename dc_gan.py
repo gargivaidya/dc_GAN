66f64e1f13da9f3614c83338ad588172b39e7adf
Gargi Vaidya <gargivaidya@tamu.edu>
2021-02-03 14:53:24 -0600

    Initial commit

---
+++ dc_gan.py
@@ -0,0 +1,91 @@
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
+
+batchSize = 64 
+imageSize = 64
+
+transform = transforms.Compose([transforms.Scale(imageSize), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]) 
+
+
+dataset = dset.CIFAR10(root = './data', download = True, transform = transform) 
+dataloader = torch.utils.data.DataLoader(dataset, batch_size = batchSize, shuffle = True, num_workers = 2) 
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+
+class G(nn.Module):
+	def __init__(self):
+		super(G, self).__init__()
+
+		self.main = nn.Sequential(
+			nn.ConvTranspose2d(100, 512, 4, 1, 0, bias = False),
+			nn.BatchNorm2d(512),
+			nn.ReLU(True),
+			nn.ConvTranspose2d(512, 256, 4, 2, 1, bias = False),
+			nn.BatchNorm2d(256),
+			nn.ReLU(True),
+			nn.ConvTranspose2d(256, 128, 4, 2, 1, bias = False),
+			nn.BatchNorm2d(128),
+			nn.ReLU(True),
+			nn.ConvTranspose2d(128, 64, 4, 2, 1, bias = False),
+			nn.BatchNorm2d(64),
+			nn.ReLU(True),
+			nn.ConvTranspose2d(64, 3, 4, 2, 1, bias = False),
+			nn.Tanh()
+			)
+
+	def forward(self, input):
+		output = self.main(input)
+		return output
+
+netG = G()
+netG.apply(weights_init)
+
+class D(nn.Module):
+	def __init__(self):
+		super(D, self).__init__()
+
+		self.main = nn.Sequential(
+			nn.Conv2d(3, 64, 4, 2, 1, bias = False),
+			nn.LeakyReLU(0.2, inplace = True),
+			nn.Conv2d(64, 128, 4, 2, 1, bias = False),
+			nn.BatchNorm2d(128),
+			nn.LeakyReLU(0.2, inplace = True),
+			nn.Conv2d(128, 256, 4, 2, 1, bias = False),
+			nn.BatchNorm2d(256),
+			nn.LeakyReLU(0.2, inplace = True),
+			nn.Conv2d(256, 512, 4, 2, 1, bias = False), 
+			nn.BatchNorm2d(512),
+			nn.LeakyReLU(0.2, inplace = True),
+			nn.Conv2d(512, 1, 4, 1, 0, bias = False),
+			nn.Sigmoid()
+			)
+	def forward(self, input):
+		output = self.main(input)
+		return output.view(-1)
+
+netD = D()
+netD.apply(weights_init)
+
+criterion = nn.BCELoss()
+optimizerD = optim.Adam(netD.parameters(), lr = 0.0002, betas = (0.5, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr = 0.0002, betas = (0.5, 0.999))
+
+for epoch in range(25):
+	for i, data in enumerate(dataloader, 0):
+		
+
+
